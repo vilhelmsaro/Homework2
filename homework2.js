54693c1741ad60a540ad417632b6caff677e53e1
vilhelmsaro <31795149+vilhelmsaro@users.noreply.github.com>
2017-09-11 15:06:57 +0400

    Commit

---
+++ homework2.js
@@ -0,0 +1,12 @@
+alert("Click on picture and follow the instruction :D");
+const NutellaPositiveOutcome = "Nutella is very tasty and makes you feel happy";
+const NutellaNegativeOutcome = "eating much Nutella will make you obese.";
+const Reality = NutellaPositiveOutcome + "but" + NutellaNegativeOutcome;
+console.log(Reality);
+
+const grade1=15;
+const grade2=10;
+const grade3=18;
+const grade4=7;
+GPA = (grade1 + grade2 + grade3 + grade4) / 4;
+console.log(GPA);
